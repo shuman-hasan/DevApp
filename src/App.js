851e7673002de05e5327c7036b10009ee1909b26
shuman-hasan <shumanhasangbd@gmail.com>
2022-02-06 14:47:29 +0600

    changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-      Development
+      <h1>Hello React !</h1>
       </header>
     </div>
   );
