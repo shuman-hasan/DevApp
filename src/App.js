1abc942e7ca24f5936db4ddf7efba8204a2e0301
shuman-hasan <shumanhasangbd@gmail.com>
2022-02-06 18:51:26 +0600

    footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Header from './Components/Header/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './Components/Footer/Footer';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Footer.css';
+
+
+function Footer() {
+  return <div>
+      <div className='container'>
+          <div className='row'>
+         <div className='col-md-6'>
+        <h3>Footer left</h3>
+         </div>
+
+         <div className='col-md-6'>
+        <h3>Footer right</h3>
+         </div>
+          </div>
+      </div>
+  </div>;
+}
+
+export default Footer;
