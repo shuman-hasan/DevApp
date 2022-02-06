23d9418e5569adcbb1c3d705c8c444d48a4812b9
shuman-hasan <shumanhasangbd@gmail.com>
2022-02-06 14:57:06 +0600

    some changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      <h1>Hello React !</h1>
-      </header>
+      <h1>Hello Reat !</h1>
     </div>
   );
 }
